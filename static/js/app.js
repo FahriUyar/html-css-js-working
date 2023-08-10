71406b1e1197dbb117d06ded92663c82300b5158
Fahri Uyar <fahriuyar32@gmail.com>
2023-08-10 16:44:09 +0300

    Projec-1 and 2

---
+++ static/js/app.js
@@ -0,0 +1,76 @@
+// GENİŞLEYEN KART
+
+const panels = document.querySelectorAll("#genisleyenkart .panel");
+
+panels.forEach(panel => {
+    panel.addEventListener("click", () => {
+
+        removeActive();
+        panel.classList.add("active");
+    });
+})
+
+function removeActive(){
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    });
+}
+
+// İLERLEYEN ADIMLAR STEP WİZARD
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll("#ilerleyenadimlar .circle");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+
+    if(currentActive > circles.length){
+        currentActive = circles.length
+    }
+
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive--;
+
+    if(currentActive < 1){
+        currentActive = 1;
+    }
+
+    update();
+});
+
+function update(){
+    circles.forEach((circle, index) => {
+        if(index < currentActive){
+            circle.classList.add('point');
+        } else{
+            circle.classList.remove('point');
+        }
+    })
+
+    const actives = document.querySelectorAll(".point");
+
+     //console.log(actives.length, circles.length);
+     //console.log(actives.length / circles.length);
+     //console.log((actives.length / circles.length) *100);
+     //console.log((actives.length -1) / (circles.length -1) *100);
+     //console.log((actives.length -1) / (circles.length -1) *100 + '%');
+
+    progress.style.width = (actives.length -1) / (circles.length -1) *100 + '%';
+
+
+    if(currentActive === 1){
+        prev.disabled = true;
+    } else if(currentActive === circles.length){
+        next.disabled = true;
+    } else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
